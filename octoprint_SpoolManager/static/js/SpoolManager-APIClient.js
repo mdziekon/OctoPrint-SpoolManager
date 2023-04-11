6eb893ed4979542b50bedbdabeb63100cafbe77a
Michał Dziekoński <michal.dziekonski+github@gmail.com>
2023-04-11 03:42:30 +0200

    GH-4 Simplify _buildRequestQuery()

--- octoprint_SpoolManager/static/js/SpoolManager-APIClient.js
+++ octoprint_SpoolManager/static/js/SpoolManager-APIClient.js
@@ -69,29 +69,24 @@ function SpoolManagerAPIClient(pluginId, baseUrl) {
     this.pluginId = pluginId;
     this.baseUrl = baseUrl;
 
-    // see https://gomakethings.com/how-to-build-a-query-string-from-an-object-with-vanilla-js/
     const _buildRequestQuery = function (data) {
-        // If the data is already a string, return it as-is
-        if (typeof (data) === 'string') return data;
-
-        // Create a query array to hold the key/value pairs
-        var query = [];
-
-        // Loop through the data object
-        for (var key in data) {
-            if (data.hasOwnProperty(key)) {
-
-                // Encode each key and value, concatenate them into a string, and push them to the array
-                query.push(encodeURIComponent(key) + '=' + encodeURIComponent(data[key]));
-            }
+        if (typeof (data) === 'string') {
+            return data;
         }
-        // Join each item in the array with a `&` and return the resulting string
-        return query.join('&');
 
+        return Object
+            .entries(data)
+            .map(([ key, value ]) => {
+                const encodedKey = encodeURIComponent(key);
+                const encodedValue = encodeURIComponent(value);
+
+                return `${encodedKey}=${encodedValue}`;
+            })
+            .join('&');
     };
 
-    const _addApiKeyIfNecessary = function(urlContext) {
-        if (UI_API_KEY){
+    const _addApiKeyIfNecessary = function (urlContext) {
+        if (UI_API_KEY) {
             urlContext = urlContext + "?apikey=" + UI_API_KEY;
         }
         return urlContext;
