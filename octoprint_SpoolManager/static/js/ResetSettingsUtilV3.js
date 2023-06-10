5638b0d2e79162235af3ebb5d69ebc8038e84ed3
Michał Dziekoński <michal.dziekonski+github@gmail.com>
2023-06-10 17:57:53 +0200

    GH-18 | Simplify button creation code

--- octoprint_SpoolManager/static/js/ResetSettingsUtilV3.js
+++ octoprint_SpoolManager/static/js/ResetSettingsUtilV3.js
@@ -39,51 +39,52 @@ function ResetSettingsUtilV3(pluginSettings) {
             }).done(function(data) {
                 let resetButton = $(RESET_BUTTON_SELECTOR);
 
-                if (data.enabled == "true") {
-                    // build-button, if necessary
-                    if (resetButton.length == 0) {
-                        $(".modal-footer > .aboutlink").after(RESET_BUTTON_HTML);
-                        resetButton = $(RESET_BUTTON_SELECTOR);
-                    }
-
-                    // add/update click action
-                    resetButton.unbind("click");
-                    resetButton.click(function() {
-                        $.ajax({
-                            url: `${API_BASEURL}plugin/${PLUGIN_ID_string}?action=resetSettings`,
-                            type: "GET",
-                        }).done(function(data) {
-                            new PNotify({
-                                title: "Default settings saved!",
-                                text: "The plugin settings have now been reset to the default values.<br>Please do a Browser reload (Strg + F5) to update all settings in the UI.",
-                                type: "info",
-                                hide: true,
-                            });
-                            // reset all values
-                            for (let propName in data) {
-                                const propValue = data[propName];
-                                // nested object, like databaseSettings? only a depth of 1
-                                if (typeof propValue == "object") {
-                                    for (let subPropName in propValue) {
-                                        subPropValue = propValue[subPropName];
-                                        pluginSettingsFromPlugin[propName][subPropName](propValue);
-                                    }
-                                } else {
-                                    pluginSettingsFromPlugin[propName](propValue);
-                                }
-                            }
-
-                            // delegate to the client. So client is able to reset/init other values
-                            mapSettingsToViewModel_function(data);
-                        });
-                    });
-
-                    resetButton.show();
-                } else {
+                if (data.enabled !== "true") {
                     if (resetButton.length) {
                         resetButton.hide();
                     }
+
+                    return;
                 }
+
+                if (!resetButton.length) {
+                    $(".modal-footer > .aboutlink").after(RESET_BUTTON_HTML);
+                    resetButton = $(RESET_BUTTON_SELECTOR);
+                }
+
+                // add/update click action
+                resetButton.unbind("click");
+                resetButton.click(function() {
+                    $.ajax({
+                        url: `${API_BASEURL}plugin/${PLUGIN_ID_string}?action=resetSettings`,
+                        type: "GET",
+                    }).done(function(data) {
+                        new PNotify({
+                            title: "Default settings saved!",
+                            text: "The plugin settings have now been reset to the default values.<br>Please do a Browser reload (Strg + F5) to update all settings in the UI.",
+                            type: "info",
+                            hide: true,
+                        });
+                        // reset all values
+                        for (let propName in data) {
+                            const propValue = data[propName];
+                            // nested object, like databaseSettings? only a depth of 1
+                            if (typeof propValue == "object") {
+                                for (let subPropName in propValue) {
+                                    subPropValue = propValue[subPropName];
+                                    pluginSettingsFromPlugin[propName][subPropName](propValue);
+                                }
+                            } else {
+                                pluginSettingsFromPlugin[propName](propValue);
+                            }
+                        }
+
+                        // delegate to the client. So client is able to reset/init other values
+                        mapSettingsToViewModel_function(data);
+                    });
+                });
+
+                resetButton.show();
             });
         });
 
