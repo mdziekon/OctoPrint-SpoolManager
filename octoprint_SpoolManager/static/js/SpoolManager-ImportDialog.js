969dbb9c5819bb9e79d98ced40046c2c77afac62
Michał Dziekoński <michal.dziekonski+github@gmail.com>
2023-06-27 02:26:41 +0200

    GH-22 | Cleanup ImportDialog code

--- octoprint_SpoolManager/static/js/SpoolManager-ImportDialog.js
+++ octoprint_SpoolManager/static/js/SpoolManager-ImportDialog.js
@@ -1,7 +1,6 @@
 
-function SpoolManagerImportDialog(){
-
-    var self = this;
+function SpoolManagerImportDialog() {
+    const self = this;
 
     this.apiClient = null;
 
@@ -16,21 +15,20 @@ function SpoolManagerImportDialog(){
     this.successMessages = ko.observable();
     this.errorMessages = ko.observable();
     this.shouldTableReload = false;
-    /////////////////////////////////////////////////////////////////////////////////////////////////// INIT
 
-    this.init = function(apiClient){
+    /////////////////////////////////////////////////////////////////////////////////////////////////// INIT
+    this.init = function(apiClient) {
         self.apiClient = apiClient;
 
         self.importSpoolItemDialog = $("#dialog_spoolManager_csvImportStatus");
-
-    }
+    };
 
     this.isInitialized = function() {
         return self.apiClient != null;
-    }
+    };
 
     /////////////////////////////////////////////////////////////////////////////////////////////////// SHOW DIALOG
-    this.showDialog = function(closeDialogHandler){
+    this.showDialog = function(closeDialogHandler) {
 
         self.closeDialogHandler = closeDialogHandler;
 
@@ -54,46 +52,52 @@ function SpoolManagerImportDialog(){
             width: 'auto',
             'margin-left': function() { return -($(this).width() /2); }
         });
-    }
+    };
 
     /////////////////////////////////////////////////////////////////////////////////////////////////// UPDATE TEXT
-    this.updateText = function(importData){
+    this.updateText = function(importData) {
+        if (importData.importStatus) {
+            return;
+        }
 
-                if (importData.importStatus) {
-                    self.importStatus(importData.importStatus);
-                    switch (importData.importStatus){
-                        case "running":
-                            self.currentLineNumber(importData.currenLineNumber);
-                            self.successMessages(importData.successMessages);
-                            errorMessage = importData.errorCollection.join(" <br> ")
-                            self.errorMessages(errorMessage);
-                            self.importInProgress(true);
-                            break;
-                        case "finished":
-                            // Final message statistic
-                            self.importInProgress(false);
-                            self.backupFilePath(importData.backupFilePath);
-                            self.backupSnapshotFilePath(importData.backupSnapshotFilePath);
-                            self.successMessages(importData.successMessages);
-                            errorMessage = importData.errorCollection.join(" <br> ")
-                            self.errorMessages(errorMessage);
+        self.importStatus(importData.importStatus);
 
-                            self.shouldTableReload = true;
+        switch (importData.importStatus) {
+            case "running": {
+                self.currentLineNumber(importData.currenLineNumber);
+                self.successMessages(importData.successMessages);
 
-                            // enable close button in dialog
-//                            self.csvImportStatusDialog.modal('hide');
-//                            self.csvImportInProgress(false);
-                            break;
-                        default:
-                    }
-                }
-                return;
-    }
+                const errorMessage = importData.errorCollection.join(" <br> ");
+
+                self.errorMessages(errorMessage);
+                self.importInProgress(true);
+                break;
+            }
+            case "finished": {
+                // Final message statistic
+                self.importInProgress(false);
+                self.backupFilePath(importData.backupFilePath);
+                self.backupSnapshotFilePath(importData.backupSnapshotFilePath);
+                self.successMessages(importData.successMessages);
+
+                const errorMessage = importData.errorCollection.join(" <br> ");
+
+                self.errorMessages(errorMessage);
+
+                self.shouldTableReload = true;
+
+                // enable close button in dialog
+                // self.csvImportStatusDialog.modal('hide');
+                // self.csvImportInProgress(false);
+                break;
+            }
+        }
+    };
 
     /////////////////////////////////////////////////////////////////////////////////////////////////// CLOSE
-    this.closeDialog  = function(){
+    this.closeDialog = function() {
         self.importSpoolItemDialog.modal('hide');
 
         self.closeDialogHandler(self.shouldTableReload);
-    }
+    };
 }
